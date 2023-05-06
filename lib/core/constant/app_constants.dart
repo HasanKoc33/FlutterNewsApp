8434d532f5ee2509c33711dfc5110c4818eb7a30
hasan koç <76748696+HasanKoc33@users.noreply.github.com>
2023-05-06 21:08:19 +0300

    Update app_constants.dart

--- lib/core/constant/app_constants.dart
+++ lib/core/constant/app_constants.dart
@@ -2,7 +2,8 @@ enum AppConstants {
   appName("News App"),
   newsHost("newsapi.org"),
   // TODO: Add your API key here
-  apiKey("401ac27513f84f94b036be2316f43d82");
+  apiKey("YOUR_API_KEY");
+
 
   final String value;
   const AppConstants(this.value);
